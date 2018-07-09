337863573fa889b6b6c6c6b5f2b7b983f1e11244
Tazbir Bhuiyan <user01.bs23@outlook.com>
2018-07-09 10:37:33 +0600

    creating mining block method

--- Module1- Create BlockChain/blockchain.py
+++ Module1- Create BlockChain/blockchain.py
@@ -65,11 +65,38 @@ class Blockchain:
         return True
     
     
+
+
+
+
+
+
+
+
+
+
 # part 2 mining our block chain
         
 #creating a web app
-        
+app = Flask(__name__)
+
+
 # creating a block chain
 
-            
-         
+blockchain = Blockchain()
+
+
+# mining a new block
+@app.route('/mine_block', methods=['GET'])            
+def mine_block():
+    previous_block = blockchain.get_previous_block()
+    previous_proof = previous_block['proof']
+    proof = blockchain.proof_of_work(previous_proof)
+    previous_hash = blockchain.hash(previous_block)
+    block= blockchain.create_block(proof,previous_hash)
+    response = {'message': 'you just mined a block!',
+                'index': block['index'],
+                'timestamp': block['timestamp'],
+                'proof': block['proof'],
+                'previous_hash': block['previous_hash']}
+    return jsonify(response), 200
